de81aeade629e71b283dcba8f2650445cd88127a
lucasborges04 <lucasbborges2004@gmail.com>
2026-02-06 12:08:12 -0300

    feat: Adicionar package de teste unitarios JUnit

---
+++ test/br/com/odontosys/service/DentistaServiceTest.java
@@ -0,0 +1,28 @@
+package br.com.odontosys.service; // Note que o pacote continua o mesmo!
+
+import br.com.odontosys.domain.Dentista;
+import config.ConfiguracaoJDBC;
+import dao.DentistaDaoH2;
+import service.DentistaService;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DentistaServiceTest {
+
+    @Test
+    public void naoDevePermitirCroDuplicado() {
+        DentistaDaoH2 dao = new DentistaDaoH2(new ConfiguracaoJDBC());
+        DentistaService service = new DentistaService(dao);
+
+        String croTeste = "CRO-" + System.currentTimeMillis();
+
+        Dentista d1 = new Dentista(null, "Original", "Geral", croTeste, "0000");
+        service.registrarDentista(d1);
+
+        Dentista d2 = new Dentista(null, "Clone", "Geral", croTeste, "1111");
+
+        assertThrows(RuntimeException.class, () -> {
+            service.registrarDentista(d2);
+        });
+    }
+}
