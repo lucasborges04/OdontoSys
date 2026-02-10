629c9bb11bd2a6e0287f4ceb93735b689873b4bf
lucasborges04 <lucasbborges2004@gmail.com>
2026-02-10 15:08:37 -0300

    feat: Cria regra de negocios para nao ter pacientes duplicados

---
+++ src/br/com/odontosys/service/PacienteService.java
@@ -0,0 +1,30 @@
+package br.com.odontosys.service;
+
+import br.com.odontosys.domain.Paciente;
+import dao.IDao;
+import java.util.List;
+
+public class PacienteService {
+
+    private final IDao<Paciente> pacienteDao;
+
+    public PacienteService(IDao<Paciente> pacienteDao) {
+        this.pacienteDao = pacienteDao;
+    }
+
+    public Paciente salvar(Paciente paciente) {
+        List<Paciente> todos = pacienteDao.buscarTodos();
+
+        for (Paciente p : todos) {
+            if (p.getEmail() != null && p.getEmail().equals(paciente.getEmail())) {
+                throw new RuntimeException("Erro: Paciente já cadastrado com o e-mail: " + paciente.getEmail());
+            }
+        }
+
+        return pacienteDao.salvar(paciente);
+    }
+
+    public List<Paciente> buscarTodos() {
+        return pacienteDao.buscarTodos();
+    }
+}
